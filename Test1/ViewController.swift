9953c35768f54e530826b5366fc4759652fc6ba9
Dim Richev <velorace@mail.ru>
2020-11-04 02:18:53 +0300

    Мои задние изменения )

--- Test1/ViewController.swift
+++ Test1/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    //override func viewDidLoad() {
+    //    super.viewDidLoad()
+    //    getImage.layer.cornerRadius = 10}
+    
+    @IBOutlet var avatar: UIImageView!
+    
+    @IBAction func getImage(_ sender: Any) {
+        let url = URL(string: "https://picsum.photos/200")
+        let data = try? Data(contentsOf: url!)
+        let image = UIImage(data: data!)
+        avatar.image = image
     }
-
-
 }
 
+
+/*@IBAction func getImage(_ sender: Any) {
+    // https://picsum.photos/200
+    let url = URL(string: "https://picsum.photos/200")
+    URLSession.shared.downloadTask(with: url!) { localUrl, response, error in
+        let data = try? Data(contentsOf: localUrl!)
+        let image = UIImage(data: data!)
+        DispatchQueue.main.async {
+    //подключаем многопоточность
+            self.avatar.image = image
+        }
+    }.resume()
+}*/
